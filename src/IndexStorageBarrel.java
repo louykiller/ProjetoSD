da6605540ebccb64b2c10c7c424eb46868740628
Pedro João Tavares <51858467+PJTavares@users.noreply.github.com>
2023-03-24 22:58:47 +0000

    Estrutura barrel feita

--- src/IndexStorageBarrel.java
+++ src/IndexStorageBarrel.java
@@ -5,44 +5,65 @@
 // idêntica ainda que possam existir avarias de omissão.
 import java.io.*;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class IndexStorageBarrel {
     // Main class
-	public static void main(String[] args) throws Exception
-	{
-        File barrel_1 = new File("a-m_barrel.txt");
-        File barrel_2 = new File("n-z_barrel.txt");
-		
-		BufferedReader br1 = new BufferedReader(new FileReader(barrel_1));
-        BufferedReader br2 = new BufferedReader(new FileReader(barrel_2));
-        
-        String st;
+    /**
+     * @param nome_barrel nome do txt onde se vai estrair informação
+     * @return retorna o barrel com a informação do txt
+     * @throws IOException
+     */
+    public static HashMap<String,String> GetInfoBarrel(String nome_barrel) throws IOException{
+        HashMap<String,String> urls = new HashMap<String,String>();
 
-        System.out.println("Urls in barrel 1!");
-		while ((st = br1.readLine()) != null)
-			System.out.println(st);
+        File barrel_1 = new File(nome_barrel);
+        BufferedReader br1 = new BufferedReader(new FileReader(barrel_1));
+        String st = "s";
+        String [] st_aux;
 
-        System.out.println("Urls in barrel 2!");
-        while ((st = br2.readLine()) != null)
-            System.out.println(st);
-    
+        while ((st = br1.readLine()) != null){
+			st_aux = st.split(",");
+            urls.put(st_aux[0],st_aux[1]);
+        }
         br1.close();
-        br2.close();
+        return urls;
+    }
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Add file to barrel");
-    
-        String fileName = myObj.nextLine();  // Read user input
-        System.out.println("File to be added: " + fileName);  // Output user input
+    /**
+     * 
+     * @param barrel barrel atual
+     * @return barrel com o novo par palavra:url
+     * @throws IOException
+     */
+    public static HashMap<String,String> AddToHash(HashMap<String,String> barrel) throws IOException{
+        HashMap<String,String> urls = new HashMap<String,String>();
+        urls = barrel;
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Word searched: ");
+        String searchedWord = myObj.nextLine();
+        
+        System.out.println("Url searched: ");
+        String searchedUrl = myObj.nextLine();
         
         myObj.close();
 
-        char firstChar = fileName.charAt(0);
-        char upperFirst = Character.toUpperCase(firstChar);
+        barrel.put(searchedWord, searchedUrl);
+        return urls;
+    }
 
-        String fullUrl = "https://"+fileName;
-        
-        try{
+    // Para separar barrel no futuro ignorar por enquanto
+
+    
+    /*
+    char firstChar = fileName.charAt(0);
+    char upperFirst = Character.toUpperCase(firstChar);
+    String fullUrl = "https://"+fileName;
+    
+    try{
             if(Character.compare(upperFirst, 'M') < 0){
                 System.out.println("Insert in first barrel!");
                 BufferedWriter bw1 = new BufferedWriter(new FileWriter("a-m_barrel.txt",true));
@@ -59,7 +80,32 @@ public class IndexStorageBarrel {
         catch(IOException exception){
             exception.printStackTrace();
         }
+        */
 
+	public static void main(String[] args) throws Exception
+	{
+        HashMap<String,String> barrel1 = new HashMap<String,String>();
+        HashMap<String,String> barrel2 = new HashMap<String,String>();
+        
+        HashMap<String,String> barrel_merged = new HashMap<String,String>();
+
+        barrel1 = GetInfoBarrel("a-m_barrel.txt");
+        barrel2 = GetInfoBarrel("n-z_barrel.txt");
+
+
+        for (Map.Entry<String, String> set : barrel1.entrySet()) {
+            barrel_merged.put(set.getKey(),set.getValue());
+        }
+
+        for (Map.Entry<String, String> set : barrel2.entrySet()) {
+            barrel_merged.put(set.getKey(),set.getValue());
+        }
+
+        System.out.println(barrel_merged);
+
+        barrel_merged = AddToHash(barrel_merged);
+        
+        System.out.println(barrel_merged);;
     }
 
 }
